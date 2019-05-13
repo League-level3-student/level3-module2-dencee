14a4c1b48dc9467cb57d988ec3adcca8a39ebcf8
Daniel Commins <dencee@users.noreply.github.com>
2019-05-13 13:48:47 -0700

    Updated quicksorter

--- src/_00_Sorting_Algorithms/QuickSorter.java
+++ src/_00_Sorting_Algorithms/QuickSorter.java
@@ -29,6 +29,7 @@ public class QuickSorter extends Sorter {
         //4. make a while loop that goes while i is less than or equal to j
 	    while( i <= j ) {
 	        display.updateDisplay();
+	        
         	//5. make a while loop that increments i while 
         	//   the array element at i is less than pivot
             while( array[ i ] < pivot ) {
