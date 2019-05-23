aa3f68fe9e9d071806c25c75c0f3e6ac37321f2f
Daniel Commins <dencee@users.noreply.github.com>
2019-05-23 14:33:37 -0700

    search recipes & more java tests

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -2,32 +2,57 @@ package _01_Searching_Algorithms;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
+    int[] ia = { 123, 55, 2, 50001, 444, 6799, 222, 150, 5555 };
+    int[] iaSorted = { 2, 55, 123, 150, 222, 444, 5555, 6799, 50001 };
+    int[] iaSpaced = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+    String[] sa = { "This", "is", "my", "testing", "array", "for", "my", "code" };
 	
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+	    assertEquals( _00_LinearSearch.linearSearch( sa, "Daniel" ), -1 );
+	    assertEquals( _00_LinearSearch.linearSearch( sa, "array" ), 4 );
+	    assertEquals( _00_LinearSearch.linearSearch( sa, "coDe" ), -1 );
+	    assertEquals( _00_LinearSearch.linearSearch( sa, "code" ), 7 );
 	}
 
 	@Test
 	public void testBinarySearch() {
+	    Arrays.sort( iaSorted );
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+	    assertEquals( _01_BinarySearch.binarySearch( iaSorted, 0, ia.length-1, 2222 ), -1 );
+	    assertEquals( _01_BinarySearch.binarySearch( iaSorted, 0, ia.length-1, 5555 ), 6 );
+	    assertEquals( _01_BinarySearch.binarySearch( iaSorted, 0, ia.length-1, 2 ), 0 );
+	    assertEquals( _01_BinarySearch.binarySearch( iaSorted, 0, 4, 444 ), -1 );
+	    assertEquals( _01_BinarySearch.binarySearch( iaSorted, 0, 5, 444 ), 5 );
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+	    assertEquals( _02_InterpolationSearch.interpolationSearch( iaSpaced, 1100 ), -1 );
+	    assertEquals( _02_InterpolationSearch.interpolationSearch( iaSpaced, 300 ), 2 );
+	    assertEquals( _02_InterpolationSearch.interpolationSearch( iaSpaced, 100 ), 0 );
+	    assertEquals( _02_InterpolationSearch.interpolationSearch( iaSpaced, 101 ), -1 );
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+        assertEquals( _03_ExponentialSearch.exponentialSearch( iaSorted, 2222 ), -1 );
+        assertEquals( _03_ExponentialSearch.exponentialSearch( iaSorted, 5555 ), 6 );
+        assertEquals( _03_ExponentialSearch.exponentialSearch( iaSorted, 2 ), 0 );
+        assertEquals( _03_ExponentialSearch.exponentialSearch( iaSorted, 444 ), 5 );
+        assertEquals( _03_ExponentialSearch.exponentialSearch( iaSorted, 50001 ), 8 );
 	}
 }
--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -6,57 +6,99 @@ import org.junit.Test;
 
 /**
  * Uncomment each test and then write the method to make it pass.
- * **/
+ **/
 
 public class _00_MoreAndMoreTests {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+    @Test
+    public void MultTest() {
 
+        assertEquals( "10 x 0 = 0", multiply( 10, 0 ) );
+        assertEquals( "10 x 10 = 100", multiply( 10, 10 ) );
+        assertEquals( "8 x 11 = 88", multiply( 8, 11 ) );
+    }
 
+    public String multiply(int num1, int num2) {
+        String result = num1 + " x " + num2 + " = " + num1 * num2;
+
+        return result;
+    }
+
+    @Test
+    public void PrimeTest() {
+
+        assertTrue( isPrime( 3 ) );
+        assertTrue( isPrime( 5 ) );
+        assertTrue( isPrime( 541 ) );
+        assertFalse( isPrime( 4 ) );
+        assertFalse( isPrime( 12 ) );
+        assertFalse( isPrime( 527 ) );
+
+    }
+
+    public boolean isPrime(int num) {
+        boolean prime = true;
+
+        if( num > 0 ) {
+            for( int i = num-1; i > 1; i-- ) {
+                if( num % i == 0 ) {
+                    prime = false;
+                }
+            }
+        } else {
+            prime = false;
+        }
+
+        return prime;
+    }
+
+    @Test
+    public void SquareTest() {
+        assertTrue( isSquare( 4 ) );
+        assertTrue( isSquare( 144 ) );
+        assertTrue( isSquare( 64 ) );
+        assertTrue( isSquare( 10201 ) );
+        assertTrue( isSquare( 1 ) );
+        assertFalse( isSquare( 3 ) );
+        assertFalse( isSquare( 22 ) );
+        assertFalse( isSquare( 143 ) );
+    }
+
+    public boolean isSquare( int i ) {
+        boolean isSquare = false;
+        
+        for( int j = 1; j <= i; j++ ) {
+            if( j * j == i ) {
+                isSquare = true;
+                break;
+            }
+        }
+        
+        return isSquare;
+    }
+    
+    @Test
+    public void CubeTest() {
+        assertTrue( isCube( 27 ) );
+        assertTrue( isCube( 216 ) );
+        assertTrue( isCube( 729 ) );
+        assertTrue( isCube( 1 ) );
+        assertFalse( isCube( 3 ) );
+        assertFalse( isCube( 22 ) );
+        assertFalse( isCube( 143 ) );
+    }
+    
+    public boolean isCube( int i ) {
+        boolean isCube = false;
+        
+        for( int j = 1; j <= i; j++ ) {
+            if( j * j * j == i ) {
+                isCube = true;
+                break;
+            }
+        }
+        
+        return isCube;
+    }
 
 }
